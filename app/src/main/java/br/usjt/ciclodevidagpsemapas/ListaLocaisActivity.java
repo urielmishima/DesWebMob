1d1a5414e1e3392daa4d95ba8322da842874e660
Uriel Mishima <uriel.hmfm@gmail.com>
2019-03-29 20:54:06 -0300

    Atividade 05

--- app/src/main/java/br/usjt/ciclodevidagpsemapas/ListaLocaisActivity.java
+++ app/src/main/java/br/usjt/ciclodevidagpsemapas/ListaLocaisActivity.java
@@ -9,6 +9,8 @@ import android.location.Location;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,30 +20,30 @@ import java.util.ArrayList;
 
 public class ListaLocaisActivity extends AppCompatActivity {
 
-    private ListView locationsListView;
+    private RecyclerView locationsRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_locais);
         Intent origemIntent = getIntent();
-        final ArrayList<Localizacao> locations = (ArrayList<Localizacao>) origemIntent.getSerializableExtra("locations");
-        locationsListView = findViewById(R.id.localizacoesListView);
-        LocalizacoesArrayAdapter adapter = new LocalizacoesArrayAdapter(this, locations);
-        locationsListView.setAdapter(adapter);
-
-        locationsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Localizacao location = locations.get(position);
-
-                Uri gmmIntentUri = Uri.parse(String.format("geo:%f,%f", location.getLongitude(), location.getLatitude()));
-
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                mapIntent.setPackage("com.google.android.apps.maps");
-                startActivity(mapIntent);
-            }
-        });
+        final ArrayList<Localizacao> localizacoes = (ArrayList<Localizacao>) origemIntent.getSerializableExtra("locations");
+        locationsRecyclerView = findViewById(R.id.localizacoesRecyclerView);
+        locationsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        LocaisRecyclerViewAdapter adapter = new LocaisRecyclerViewAdapter(localizacoes);
+        locationsRecyclerView.setAdapter(adapter);
+//        locationsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Localizacao location = locations.get(position);
+//
+//                Uri gmmIntentUri = Uri.parse(String.format("geo:%f,%f", location.getLongitude(), location.getLatitude()));
+//
+//                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+//                mapIntent.setPackage("com.google.android.apps.maps");
+//                startActivity(mapIntent);
+//            }
+//        });
 
 
     }
